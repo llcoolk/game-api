971a9b440438c744de84ea578c9aa03a05c44f77
Leen Kim <llcoolk@MACBKPROi7.local>
2018-07-29 18:15:26 -0700

    added teams table

--- src/index.js
+++ src/index.js
@@ -5,48 +5,61 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  'dizpuuiq_llcoolk',
-  'dizpuuiq_llcoolk',
-  'llcoolk!', {
-    host: 'dionimercado.com',
-    dialect: 'mysql',
+  "dizpuuiq_llcoolk",
+  "dizpuuiq_llcoolk",
+  "llcoolk!",
+  {
+    host: "dionimercado.com",
+    dialect: "mysql",
     port: 3306,
-    operatorsAliases: false,
+    operatorsAliases: false
   }
 );
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err);
+    console.error("Unable to connect to the database:", err);
   });
 
-
-const Player = sequelize.define(
-  'players', {
-
-    firstName: {
-      type: Sequelize.STRING
-    },
-    lastName: {
-      type: Sequelize.STRING
-    },
-    age: {
-      type: Sequelize.INTEGER
-    }
+const Team = sequelize.define("Teams", {
+  teamName: {
+    type: Sequelize.STRING
   }
-);
+});
 
-// Player.sync({
+const Player = sequelize.define("Players", {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  },
+  age: {
+    type: Sequelize.INTEGER
+  }
+});
+
+Player.belongsTo(Team);
+Team.hasMany(Player);
+
+Team.sync({
+  force: true
+}).then(() => {
+  return Team.create({
+    teamName: "Angels"
+  });
+});
+
+// Players.sync({
 //   force: true
 // }).then(() => {
-//   // Table created
 //   return Player.create({
 //     firstName: 'Justin',
 //     lastName: 'Kim',
@@ -54,86 +67,77 @@ const Player = sequelize.define(
 //   });
 // });
 
-// User.findOne().then(user => {
-//   console.log(user.get('firstName'));
-// });
-
-
-app.get('/', (req, res) => {
-  Player.findAll({})
-    .then(players => {
-      res.json(players);
-    })
+app.get("/player", (req, res) => {
+  Player.findAll({}).then(players => {
+    res.json(players);
+  });
 });
 
-app.get('/player/:id', (req, res) => {
+app.get("/player/:id", (req, res) => {
   Player.findOne({
     where: {
-      id: req.params.id,
-    },
+      id: req.params.id
+    }
   }).then(player => {
     res.json(player);
   });
 });
 
-app.post('/player', (req, res) => {
+app.post("/player", (req, res) => {
   const player = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     age: req.body.age
   };
   Player.create(player)
-    .then(player = res.json(player))
-    .catch(err => res.json({
-      Error: err
-    }));
+    .then((player = res.json(player)))
+    .catch(err =>
+      res.json({
+        Error: err
+      })
+    );
 });
 
-app.put('/player/:id', (req, res) => {
-  const {
-    firstName,
-    lastName,
-    age
-  } = req.body;
+app.put("/player/:id", (req, res) => {
+  const { firstName, lastName, age } = req.body;
 
   const player = {};
 
   if (firstName) {
-    player.firstName = firstName
+    player.firstName = firstName;
   }
   if (lastName) {
-    player.lastName = lastName
+    player.lastName = lastName;
   }
   if (age) {
-    player.age = age
+    player.age = age;
   }
 
   Player.update(player, {
-      where: {
-        id: req.params.id,
-      },
-    })
+    where: {
+      id: req.params.id
+    }
+  })
     .then(player => res.json(player))
     .catch(err =>
       res.json({
-        Error: err,
+        Error: err
       })
     );
 });
 
-app.delete('/player/:id', (req, res) => {
+app.delete("/player/:id", (req, res) => {
   Player.destroy({
-      where: {
-        id: req.params.id,
-      },
-    })
+    where: {
+      id: req.params.id
+    }
+  })
     .then(players => res.json(players))
     .catch(err =>
       res.json({
-        Error: err,
+        Error: err
       })
     );
 });
 
-
-app.listen(5000, () => console.log("Server running at http://localhost:5000"))
+app.listen(5000, () => console.log("Server running at http://localhost:5000"));
